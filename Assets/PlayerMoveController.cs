41d5c0c77f82620ab2c39ef5f6e51c91a0140b03
Efren Christofer <111559192+Efrennnn@users.noreply.github.com>
2024-06-11 00:11:29 +0700

    Working Damaging

--- Assets/PlayerMoveController.cs
+++ Assets/PlayerMoveController.cs
@@ -3,101 +3,96 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMoveController : MonoBehaviour
-{
+public class PlayerMoveController : MonoBehaviour{
     public float moveSpeed = 1f;
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
     public SwordAttackArea swordAttack;
 
     private Vector2 movementInput;
-    SpriteRenderer spriteRenderer;
+    private SpriteRenderer spriteRenderer;
     private Rigidbody2D rb;
-    Animator animator;
+    private Animator animator;
     private List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
-    bool canMove = true;
+    private bool canMove = true;
 
-    void Start()
-    {
+    void Start(){
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate(){
-        if (canMove) {
-        if (movementInput != Vector2.zero){
-            bool success = TryMove(movementInput);
+        if (canMove){
+            if (movementInput != Vector2.zero){
+                bool success = TryMove(movementInput);
 
-            if(!success ) {
-                success = TryMove(new Vector2(movementInput.x, 0));
-            }
+                if (!success)
+                {
+                    success = TryMove(new Vector2(movementInput.x, 0));
+                }
 
-            if(!success) {
+                if (!success)
+                {
                     success = TryMove(new Vector2(0, movementInput.y));
+                }
+
+                animator.SetBool("IsMoving", success);
+                animator.SetFloat("MoveX", movementInput.x);
+                animator.SetFloat("MoveY", movementInput.y);
+            } else {
+                animator.SetBool("IsMoving", false);
             }
 
-            animator.SetBool("IsMoving", success);
-            animator.SetFloat("MoveX", movementInput.x);
-            animator.SetFloat("MoveY", movementInput.y);
-
-        } else {
-            animator.SetBool("IsMoving", false);
+            if (movementInput.x < 0){
+                spriteRenderer.flipX = true;
+            } else if (movementInput.x > 0){
+                spriteRenderer.flipX = false;
+            }
         }
-
-        if(movementInput.x < 0) { 
-            spriteRenderer.flipX = true;
-        } else if (movementInput.x > 0) {
-            spriteRenderer.flipX = false;
-        }
-
-    }
     }
 
-    private bool TryMove(Vector2 direction) {
-    
-        if(direction != Vector2.zero) {
-        int count = rb.Cast(
-            direction, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset
-        );
+    private bool TryMove(Vector2 direction){
+        if (direction != Vector2.zero){
+            int count = rb.Cast(
+                direction, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset
+            );
 
-        if(count == 0) {
-            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
-            return true;
-        } else {
-            return false;
-        }
-
+            if (count == 0){
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            } else {
+                return false;
+            }
         } else {
             return false;
         }
     }
 
-    void OnMove(InputValue movementValue)
-    {
+    void OnMove(InputValue movementValue){
         movementInput = movementValue.Get<Vector2>();
     }
 
-    void OnFire() {
+    void OnFire(){
         animator.SetTrigger("Attack");
     }
 
-    public void SwordAttackArea() {
+    public void SwordAttackArea(){
         LockMovement();
-        if(spriteRenderer.flipX == true) {
+        if (spriteRenderer.flipX == true){
             swordAttack.AttackLeft();
         } else {
             swordAttack.AttackRight();
         }
     }
 
-    public void EndSwordAttack() {
+    public void EndSwordAttack(){
         UnlockMovement();
         swordAttack.StopAttack();
     }
 
-    public void LockMovement() {
+    public void LockMovement(){
         canMove = false;
     }
 
--- Assets/SwordAttackArea.cs
+++ Assets/SwordAttackArea.cs
@@ -6,41 +6,39 @@ public class SwordAttackArea : MonoBehaviour
 {
     public float damage = 3;
     public Collider2D swordCollider;
-    Vector2 rightAttackOffset;
+    private Vector2 rightAttackOffset;
 
-    // Start is called before the first frame update
-    private void Start()
-    {
-        rightAttackOffset = transform.position;
+    private void Start(){
+        rightAttackOffset = transform.localPosition;
     }
 
-
-    public void AttackRight() {
-        swordCollider.enabled = true;
-        transform.localPosition = new Vector3 (rightAttackOffset.x * -1, rightAttackOffset.y);
-        print("Right");
-    }
-
-    public void AttackLeft() {
+    public void AttackRight(){
         swordCollider.enabled = true;
         transform.localPosition = rightAttackOffset;
-        print("Left");
+        Debug.Log("AttackRight triggered");
     }
 
-    public void StopAttack() {
+    public void AttackLeft(){
+        swordCollider.enabled = true;
+        transform.localPosition = new Vector3(rightAttackOffset.x * -1, rightAttackOffset.y);
+        Debug.Log("AttackLeft triggered");
+    }
+
+    public void StopAttack(){
         swordCollider.enabled = false;
-
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Enemy") {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("SwordAttackArea OnTriggerEnter2D triggered");
 
+        if (other.CompareTag("Enemy")){
             Enemy enemy = other.GetComponent<Enemy>();
 
-            if (enemy != null) {
+            if (enemy != null){
+                Debug.Log("Damaging enemy");
                 enemy.Health -= damage;
             }
         }
     }
-
 }
